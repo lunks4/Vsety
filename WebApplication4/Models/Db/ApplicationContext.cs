86bb37357c9c03913f6477116fd3a6d7f7132810
lunks4 <140511228+lunks4@users.noreply.github.com>
2024-08-13 20:28:44 +0300

    Добавил еще одно представление и еще немного

---
+++ WebApplication4/Models/Db/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Models.Db
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public ApplicationContext() => Database.EnsureCreated();
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+    }
+}
---
+++ WebApplication4/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public int age { get; set; }
+    }
+}
